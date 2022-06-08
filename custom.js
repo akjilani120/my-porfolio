d4acdded4e1e12afae62606f192a1f8f2ed624c3
akjilani120 <abdulkaderjilani120@gmail.com>
2022-06-09 03:12:17 +0600

    added my potfolio

---
+++ custom.js
@@ -0,0 +1,17 @@
+$("#example").typer({
+    strings: [
+      "Junior Full Stack Web Developer",
+      "Junior Full stack MERN Developer",
+      
+    ],
+    typeSpeed: 150,
+    backspaceSpeed: 50,
+    backspaceDelay: 500,
+    repeatDelay: 1000,
+    repeat: true,
+    autoStart: true,
+    startDelay: 100,
+  });
+
+ 
+  
