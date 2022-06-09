f6660ab12e5a84d302a43d1e30560c64022e1880
akjilani120 <abdulkaderjilani120@gmail.com>
2022-06-09 23:00:22 +0600

    added send email

--- custom.js
+++ custom.js
@@ -27,6 +27,24 @@
       lat: 23.684994,
       lng: 90.356331
     });
+    (function(){
+      emailjs.init("jURxjrUHMky1qUUlk");
+   })();
+  window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_hkumgow', 'template_nd3xbpg', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
+
   });
  
   
